c154819509b79065222072067834389eff36d7f2
samiuallahasad <117480429+samiuallahasad@users.noreply.github.com>
2022-11-22 02:23:17 -0800

    Add files via upload

---
+++ deque.java
@@ -0,0 +1,13 @@
+package Arraydeque;
+import java.util.ArrayDeque;
+public class deque {
+
+    public static void main(String[] args) {
+      ArrayDeque deque = new ArrayDeque();      
+      deque.add("samiullah asad");
+      System.out.println(deque);
+            
+              
+    }
+    
+}
