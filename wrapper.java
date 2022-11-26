e8dea749bce985e585be58cee6270a1c811f8858
samiuallahasad <117480429+samiuallahasad@users.noreply.github.com>
2022-11-26 11:28:02 -0800

    Add files via upload

---
+++ wrapper.java
@@ -0,0 +1,13 @@
+package rapperclass;
+
+public class wrapper {
+  public static void main(String[] args) {
+    Integer mInt = 5;
+    Double mDouble = 5.99;
+    Character mChar = 'A';
+    System.out.println(mInt);
+    System.out.println(mDouble);
+    System.out.println(mChar);
+  }
+   
+}
