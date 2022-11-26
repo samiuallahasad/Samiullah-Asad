b570b730bce780431270bf0bbbc750c153f3893e
samiuallahasad <117480429+samiuallahasad@users.noreply.github.com>
2022-11-26 11:30:10 -0800

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,14 @@
+package collectoins;
+import java.util.LinkedList;
+public class NewClass {
+    
+    public static void main(String[] args) {
+      LinkedList linked =new LinkedList();
+      linked.add("samiullah ");
+      linked.add("zarif");
+      linked.add(23456);
+      
+      System.out.println(linked);
+    }
+    
+}
