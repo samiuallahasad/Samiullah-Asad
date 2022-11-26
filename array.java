3f32f1fd4331ac8bfb0731df8890e70eaebf0c64
samiuallahasad <117480429+samiuallahasad@users.noreply.github.com>
2022-11-26 11:25:15 -0800

    Add files via upload

---
+++ array.java
@@ -0,0 +1,24 @@
+package arraylist;
+import java.util.ArrayList;
+public class array {
+
+   
+    public static void main(String[] args) {
+     ArrayList<String> marray= new ArrayList<String>();
+     
+
+     marray.add("kandahar");
+     marray.add("helmand");
+     marray.add("kabul");
+     marray.add("herat");
+     marray.add("zabul");
+     
+    
+System.out.println(marray.get(4));
+System.out.println(marray.set(2,"myname"));
+
+System.out.println(marray.size());
+System.out.println(marray.(2));
+
+
+    }}
